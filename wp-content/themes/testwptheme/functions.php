a6d91addd9fbfca26e34c37d3b0822158b8f358a
Conor Lewis-Hobbs <105722475+ConorLewis-Hobbs@users.noreply.github.com>
2022-06-27 14:55:56 +0100

    dynamic style sheet data

--- wp-content/themes/testwptheme/functions.php
+++ wp-content/themes/testwptheme/functions.php
@@ -1,7 +1,10 @@
 <?php
 
 function conor_register_styles(){
-    wp_enqueue_style("conor-bootstrap", get_template_directory_uri() . "/style.css", array(), "1.0", "all");
+    $version = wp_get_theme()->get( "version" );
+    wp_enqueue_style("conor-style", get_template_directory_uri() . "/theme-template/style.css", array("conor-bootstrap"), "$version", "all");
+    wp_enqueue_style("conor-bootstrap", "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), "4.4.1", "all");
+    wp_enqueue_style("conor-fontawsome","https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), "5.13.0", "all");
 }
 
 add_action("wp_enqueue_scripts", "conor_register_styles");
