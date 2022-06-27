218f703f1e268004e899b3aae969ad226758f28b
Conor Lewis-Hobbs <105722475+ConorLewis-Hobbs@users.noreply.github.com>
2022-06-27 13:51:05 +0100

    linking style sheet using php

---
+++ wp-content/themes/testwptheme/functions.php
@@ -0,0 +1,9 @@
+<?php
+
+function conor_register_styles(){
+    wp_enqueue_style("conor-bootstrap", get_template_directory_uri() . "/style.css", array(), "1.0", "all");
+}
+
+add_action("wp_enqueue_scripts", "conor_register_styles");
+
+?>
