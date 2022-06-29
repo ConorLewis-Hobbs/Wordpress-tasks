1ef0493ee27156cbff5a7404fabbac94029990f1
Conor Lewis-Hobbs <105722475+ConorLewis-Hobbs@users.noreply.github.com>
2022-06-29 14:08:07 +0100

    nav chnages

--- wp-content/themes/testwptheme/functions.php
+++ wp-content/themes/testwptheme/functions.php
@@ -1,4 +1,20 @@
 <?php
+function dynamic_title(){
+//adds dynamic title tag support
+add_theme_support("title-tag");
+}
+
+add_action("after_setup_theme", "dynamic_title");
+
+function menus(){
+    $locations = array(
+        "primary" => "desktop primary left sidebar",
+        "foorer" => "footer menu items",
+    );
+    register_nav_menus($locations);
+}
+
+add_action("init","menus");
 
 function conor_register_styles(){
     $version = wp_get_theme()->get( "version" );
@@ -8,5 +24,15 @@ function conor_register_styles(){
 }
 
 add_action("wp_enqueue_scripts", "conor_register_styles");
+
+function conor_register_scripts(){
+
+    wp_enqueue_script("conor-jquery", "https://code.jquery.com/jquery-3.4.1.slim.min.js", array(), "3.4.1", "true");
+    wp_enqueue_script("conor-popper", "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js", array(), "16.0", "true");
+    wp_enqueue_script("conor-bootstrap", "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js", array(), "4.4.1", "true");
+    wp_enqueue_script("conor-main", get_template_directory_uri() . "/assets/js/main.js", array(), "1,0", "true");
+}
+
+add_action("wp_enqueue_scripts", "conor_register_scripts");
 
 ?>
